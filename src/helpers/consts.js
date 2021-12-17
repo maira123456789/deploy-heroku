b7af3ddcac113aa258b0fe265f3e74804f2bed4d
M Uzakbekov <muzakbekov@MacBook-Air-M.local>
2021-12-17 11:44:28 +0600

    fixed4

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -1,3 +1,3 @@
-export const PRODUCTS_API = "https://watches-watches.herokuapp.com//apiproducts";
+export const PRODUCTS_API = "https://watches-watches.herokuapp.com/apiproducts";
 
-export const BRANDS_API = "https://watches-watches.herokuapp.com//apibrands";
+export const BRANDS_API = "https://watches-watches.herokuapp.com/apibrands";
